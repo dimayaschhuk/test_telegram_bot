d270c11dcae4735cbc31510ee0efd8b38f2841d1
dmytro <dimaworkyaschhuk@gmail.com>
2019-01-21 12:41:14 +0200

    fix

--- config/botman/telegram.php
+++ config/botman/telegram.php
@@ -11,5 +11,6 @@ return [
     | the chatbot through Telegram.
     |
     */
-    'token' => env('TELEGRAM_TOKEN','779486044:AAGWMV_GKkmMJ-_J7DIWEZ7PPCXHsxvKdKA'),
+    'token' => env('TELEGRAM_TOKEN','779486044:AAE9G5EjIGrS-NCrP0Yswdttw2d-rmhxEi4'),
 ];
+
